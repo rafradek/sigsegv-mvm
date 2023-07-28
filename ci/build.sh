97afcb51eedbe7febc555dc93678a7d7f70038fe
stephanie sappho lenzo <sappho@sappho.io>
2023-07-28 05:41:55 -0400

    use docker instead of podman

--- ci/build.sh
+++ ci/build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 
-
 # we do this so that we can be agnostic about where we're invoked from
-
 # meaning you can exec this script anywhere and it should work the same
-
 thisiswhereiam=${BASH_SOURCE[0]}
-
 # this should be /whatever/directory/structure/Open-Fortress-Source
-
 script_folder=$( cd -- "$( dirname -- "${thisiswhereiam}" )" &> /dev/null && pwd )
-
-
 
 # this should be /whatever/directory/structure/[sdkmod-source]/build
 
@@ -22,7 +15,17 @@ img="ubuntu:20.04"
 echo ${thisiswhereiam}
 echo ${script_folder}
 
-podman run -it \
+#podman run -it \
+#--mount type=bind,source=${script_folder}/../,target=/mnt/sigsegv-mvm \
+#${img} \
+#bash /mnt/sigsegv-mvm/ci/_docker_script.sh
+
+
+docker run -it \
 --mount type=bind,source=${script_folder}/../,target=/mnt/sigsegv-mvm \
 ${img} \
 bash /mnt/sigsegv-mvm/ci/_docker_script.sh
+
+# investigate
+# --user "$(id -u):$(id -g)"
+# for fixing perm issues...
