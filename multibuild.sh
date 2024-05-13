0d8fb135bb753377af116695af26069182e0b204
rafradek <rafradek@hotmail.com>
2024-05-13 10:59:49 +0200

    update ci

--- multibuild.sh
+++ multibuild.sh
@@ -2,28 +2,34 @@
 
 pushd build/release
 	(
-		ambuild
-		cd package
-		rm -f ../package-linux.zip
-		type zip >/dev/null 2>&1 && zip -y -q -FSr ../package-linux.zip *
+		ambuild &&
+		(
+			cd package
+			rm -f ../package-linux.zip
+			type zip >/dev/null 2>&1 && zip -y -q -FSr ../package-linux.zip *
+		)
 	) &
 popd
 
 pushd build/release/optimize-only
 	(
-		ambuild
-		cd package
-		rm -f ../../package-linux-optimize-only.zip
-		type zip >/dev/null 2>&1 && zip -y -q -FSr ../../package-linux-optimize-only.zip *
+		ambuild &&
+		(
+			cd package
+			rm -f ../../package-linux-optimize-only.zip
+			type zip >/dev/null 2>&1 && zip -y -q -FSr ../../package-linux-optimize-only.zip *
+		)
 	) &
 popd
 
 pushd build/release/no-mvm
 	(
-		ambuild
-		cd package
-		rm -f ../../package-linux-no-mvm.zip
-		type zip >/dev/null 2>&1 && zip -y -q -FSr ../../package-linux-no-mvm.zip *
+		ambuild &&
+		(
+			cd package
+			rm -f ../../package-linux-no-mvm.zip
+			type zip >/dev/null 2>&1 && zip -y -q -FSr ../../package-linux-no-mvm.zip *
+		)
 	) &
 popd
 
