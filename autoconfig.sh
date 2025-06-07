eb88ab60692662b5e3caad303f36e7991dd741cc
Rafał Radtke <rafradek@hotmail.com>
2025-06-07 13:45:57 +0200

    dependencies

--- autoconfig.sh
+++ autoconfig.sh
@@ -5,24 +5,27 @@ PROJECT_DIR=$(dirname $(realpath $0))
 echo $PROJECT_DIR
 PATHS="--hl2sdk-root=$PROJECT_DIR/../alliedmodders --mms-path=$PROJECT_DIR/../alliedmodders/metamod-source --sm-path=$PROJECT_DIR/../alliedmodders/sourcemod"
 
+export CC=${CC:=gcc}
+export CXX=${CXX:=g++}
+
 mkdir -p build
 cd build
- 	CC=gcc CXX=g++ $CONFIGURE $PATHS --sdks=tf2 --enable-debug --exclude-mods-debug --enable-optimize --exclude-mods-visualize --exclude-vgui
+ 	$CONFIGURE $PATHS --sdks=tf2 --enable-debug --exclude-mods-debug --enable-optimize --exclude-mods-visualize --exclude-vgui
 cd ..
 
 mkdir -p build/release
 pushd build/release
-	CC=gcc CXX=g++ $CONFIGURE $PATHS --targets=x86_64,x86 --sdks=tf2,css,sdk2013 --build-all --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
+	$CONFIGURE $PATHS --targets=x86_64,x86 --sdks=tf2,css,sdk2013 --build-all --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
 popd
 
 mkdir -p build/x86
 pushd build/x86
-	CC=gcc CXX=g++ $CONFIGURE $PATHS --targets=x86 --sdks=tf2 --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
+	$CONFIGURE $PATHS --targets=x86 --sdks=tf2 --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
 popd
 
 mkdir -p build/x64
 pushd build/x64
-	CC=gcc CXX=g++ $CONFIGURE $PATHS --targets=x86_64 --sdks=tf2 --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
+	$CONFIGURE $PATHS --targets=x86_64 --sdks=tf2 --enable-optimize --exclude-mods-debug --exclude-mods-visualize --exclude-vgui
 popd
 # mkdir -p build/release/optimize-only
 # pushd build/release/optimize-only
